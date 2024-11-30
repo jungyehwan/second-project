91da012bd29d165bd7f39e1702a23df80a9ddb46
Yehwan Jung <qseqse1532@naver.com>
2024-12-01 00:18:36 +0900

    파일업로드

--- src/main/java/com/study/board/controller/BoardController.java
+++ src/main/java/com/study/board/controller/BoardController.java
@@ -6,6 +6,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.multipart.MultipartFile;
 
 @Controller
 public class BoardController {
@@ -18,9 +19,13 @@ public class BoardController {
 
 
     @PostMapping("/board/writepro")
-    public String boardWritePro(Board board) {
-        boardService.write(board);
-        return "";
+    public String boardWritePro(Board board, Model model, @RequestParam(name = "file") MultipartFile file) throws Exception {
+        boardService.write(board, file);
+
+
+        model.addAttribute("message", "글 작성이 완료되었습니다.");
+        model.addAttribute("searchUrl","/board/list");
+        return "message";
     }
 
     @GetMapping("/board/list")
@@ -46,7 +51,7 @@ public class BoardController {
     }
 
     @GetMapping("/board/modify/{id}")
-    public String boardModify(@PathVariable("id") Integer id,
+    public String boardModify(@PathVariable("id")  Integer id,
                               Model model) {
 
         model.addAttribute("board", boardService.boardView(id));
@@ -54,13 +59,12 @@ public class BoardController {
     }
 
     @PostMapping("/board/update/{id}")
-    public String boardUpdate(@PathVariable("id") Integer id, Board board) {
+    public String boardUpdate(@PathVariable("id") Integer id, Board board,@RequestParam(name = "file") MultipartFile file) throws Exception {
 
         Board boardTemp = boardService.boardView(id);
         boardTemp.setTitle(board.getTitle());
         boardTemp.setContent(board.getContent());
-
-        boardService.write(boardTemp);
+        boardService.write(boardTemp, file);
         return "redirect:/board/list";
     }
 
